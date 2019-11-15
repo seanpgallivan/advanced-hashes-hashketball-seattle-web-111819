0579ecb9aeff9d106e765946daf002c3c3799889
Sean Gallivan <seanpgallivan@hotmail.com>
2019-11-15 05:37:53 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,230 @@
-# Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => [{
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
+      },{
+        :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+      },{
+        :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds =>  19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
+      },{
+        :player_name => "Mason Plumlee",
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 11,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
+      },{
+        :player_name => "Jason Terry",
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
+      }]
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => [{
+        :player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
+      },{
+        :player_name => "Bismack Biyombo",
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      },{
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+      },{
+        :player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
+      },{
+        :player_name => "Kemba Walker",
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 7,
+        :blocks => 5,
+        :slam_dunks => 12
+      }]
+    }
+  }
+end
 
+def player_stats(name)
+  new = {}
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if stats[:player_name] == name
+        new = stats
+        new.shift
+      end
+    end
+  end
+  new
+end
 
+def num_points_scored(name)
+  player_stats(name)[:points]
+end
 
+def shoe_size(name)
+  player_stats(name)[:shoe]
+end
 
+def team_colors(name)
+  game_hash.each do |side, team|
+    return team[:colors] if team[:team_name] == name
+  end
+end
+
+def team_names
+  new = []
+  game_hash.each do |side, team|
+    new << team[:team_name]
+  end
+  new
+end
+
+def player_numbers(name)
+  nums = []
+  game_hash.each do |side, team|
+    if team[:team_name] == name
+      team[:players].each do |stats|
+        nums << stats[:number]
+      end
+    end
+  end
+  nums
+end
+
+def big_shoe_rebounds
+  new = nil
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if !new || stats[:shoe] > new[0]
+        new = [stats[:shoe], stats[:player_name]]
+      end
+    end
+  end
+  player_stats(new[1])[:rebounds]
+end
+
+def most_points_scored
+  new = nil
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if !new || stats[:points] > new[0]
+        new = [stats[:points], stats[:player_name]]
+      end
+    end
+  end
+  new[1]
+end
+
+def winning_team
+  score = [0, 0]
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if side.to_s == "home"
+        score[0] += stats[:points]
+      else
+        score[1] += stats[:points]
+      end
+    end
+  end
+  if score[0] > score[1]
+    return game_hash[:home][:team_name]
+  else
+    return game_hash[:away][:team_name]
+  end
+end
+
+def player_with_longest_name
+  new = nil
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if !new || stats[:player_name].length > new[0]
+        new = [stats[:player_name].length, stats[:player_name]]
+      end
+    end
+  end
+  new[1]
+end
+      
+def long_name_steals_a_ton?
+  new = nil
+  game_hash.each do |side, team|
+    team[:players].each do |stats|
+      if !new || stats[:steals] > new[0]
+        new = [stats[:steals], stats[:player_name]]
+      end
+    end
+  end
+  new[1] == player_with_longest_name
+end
 
 
 
